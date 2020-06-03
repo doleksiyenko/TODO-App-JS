8b82fb1ce2ef3b5965c9b094ce858e3ada6c5972
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-06-02 23:12:33 -0400

    Added character counting functionality

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const addNote = document.getElementById('add-todo');
-const charLimit = document.getElementById('char-limit');
-const todoEntry = document.getElementById('todo-entry');
+const addNote = document.getElementById("add-todo");
+const charLimit = document.getElementById("char-limit");
+const todoEntry = document.getElementById("todo-entry");
 
-addNote.onclick = function(e) {
-    e.preventDefault();
-}
+todoEntry.oninput = () => {
+    let taskLength = todoEntry.value.length;
+    charLimit.textContent = `${taskLength}/50`;
+    charLimit.style.color = "white";
+    // set the colour of the text based on the length of input
+    if (taskLength < 10) {
+        charLimit.textContent = `0${taskLength}/50`;
+    } else if (taskLength >= 25 && taskLength < 50) {
+        charLimit.style.color = "yellow";
+    } else if (taskLength == 50) {
+        charLimit.style.color = "red";
+    }
+};
+
+addNote.onclick = (event) => event.preventDefault();
