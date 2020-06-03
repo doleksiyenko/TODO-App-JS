cf2493a49cfcd77caa0c795be6069925d949f243
doleksiyenko <51185857+doleksiyenko@users.noreply.github.com>
2020-06-02 22:45:28 -0400

    Created basic interface (intro text, form)

---
+++ app.js
@@ -0,0 +1,7 @@
+const addNote = document.getElementById('add-todo');
+const charLimit = document.getElementById('char-limit');
+const todoEntry = document.getElementById('todo-entry');
+
+addNote.onclick = function(e) {
+    e.preventDefault();
+}
